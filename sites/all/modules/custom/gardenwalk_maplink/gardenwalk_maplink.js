f0cad3e08685230a06f91ca6f0dd1a8cc65ca250
Jeff Schuler <jeff@websubstrate.com>
2012-06-28 00:37:36 -0400

    maplink: fix marker select highlighting from outside click

--- sites/all/modules/custom/gardenwalk_maplink/gardenwalk_maplink.js
+++ sites/all/modules/custom/gardenwalk_maplink/gardenwalk_maplink.js
@@ -25,7 +25,8 @@ Drupal.gardenwalk_maplink = {
       var layer_id = Drupal.gardenwalk_maplink.nids[nid][0];
       var index = Drupal.gardenwalk_maplink.nids[nid][1];
       var feature = $('div.openlayers-map-neighborhood_map').data('openlayers').openlayers.layers[layer_id].features[index];
-      Drupal.openlayers_plus_behavior_popup.openPopup(feature);
+      var selectControl = $('div.openlayers-map-neighborhood_map').data('openlayers').openlayers.controls[0];
+      Drupal.openlayers_plus_behavior_popup.openPopup(feature, selectControl);
     } 
     return false;
   }
