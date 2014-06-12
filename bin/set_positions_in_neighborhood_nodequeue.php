7513be9b54afe52218a4881c7b70e0332e8389b6
Jeff Schuler <jeff@websubstrate.com>
2014-06-12 13:12:25 -0400

    Updated marker ordering script for Slavic Village.

--- bin/set_positions_in_neighborhood_nodequeue.php
+++ bin/set_positions_in_neighborhood_nodequeue.php
@@ -16,9 +16,11 @@ nodequeue_queue_swap
 */
 
 $qid = 4; // Neighborhood Queue
-$sqid = 4; // Detroit Shoreway Subqueue
 
-$order_by_marker = array('A1', 'A73', 'A30', 'A57', 'A29', 'A39', 'A65', 'A43', 'A41', 'A77', 'A27', 'A9', 'A26', 'A25', 'A24', 'A18', 'A10', 'A8', 'A7', 'A51', 'A23', 'A2', 'A56', 'A68', 'A53', 'A13', 'A17', 'A35', 'A12', 'A28', 'A61', 'A54', 'A19', 'A59', 'A48', 'A71', 'A20', 'A52', 'A58', 'A37', 'A47', 'A33', 'A70', 'A32', 'A64', 'A62', 'A55', 'A72', 'A67', 'A45', 'A80', 'A81', 'A79', 'A69', 'A22', 'A31', 'A15', 'A14', 'A21', 'A66', 'A42', 'A4', 'A78', 'A5', 'A6', 'A78', 'A75', 'A44', 'A16', 'A40', 'A60');
+//$sqid = 4; // Detroit Shoreway Subqueue
+$sqid = 10; // Slavic Village
+
+$order_by_marker = array('D1', 'D33', 'D10', 'D39', 'D11', 'D19', 'D13', 'D34', 'D35', 'D37', 'D30', 'D40', 'D36', 'D4', 'D5', 'D25', 'D3', 'D24', 'D20', 'D12', 'D2', 'D14', 'D15', 'D21', 'D23', 'D29', 'D16', 'D27', 'D31', 'D6', 'D18', 'D8', 'D38', 'D26', 'D9', 'D28', 'D22', 'D17');
 
 print_r('$order_by_marker:');
 print_r($order_by_marker);
@@ -61,5 +63,6 @@ foreach ($reverse_order_nids as $nid) {
 //print_r('$current_positions:');
 //print_r($current_positions);
 
+//@TODO: Re-save all points in neighborhood to update computed field markers
 
 ?>
