fac33fa45fb2a40acf0cf30abdc0a3967de9e62a
Jeff Schuler <jeff@websubstrate.com>
2015-06-19 15:56:47 -0400

    _custom/theme: Use gardenwalk theme on neighborhood nodequue ordering page, and override its display to show Map Marker Label.

--- sites/all/themes/gardenwalk/template.php
+++ sites/all/themes/gardenwalk/template.php
@@ -64,3 +64,78 @@ function gardenwalk_preprocess_page(&$vars) {
     }
   }
 }
+
+/**
+ * Implements theme_nodequeue_arrange_subqueue_form_table().
+ *
+ * Add a column for Marker Label to help when ordering points.
+ */
+function gardenwalk_nodequeue_arrange_subqueue_form_table($variables) {
+  $form = $variables['form'];
+
+  $output = '';
+
+  // Get css to hide some of the help text if javascript is disabled.
+  drupal_add_css(drupal_get_path('module', 'nodequeue') . '/nodequeue.css');
+
+  $table_id = 'nodequeue-dragdrop-' . $form['#subqueue']['sqid'];
+  $table_classes = array(
+    'nodequeue-dragdrop',
+    'nodequeue-dragdrop-qid-' . $form['#subqueue']['qid'],
+    'nodequeue-dragdrop-sqid-' . $form['#subqueue']['sqid'],
+    'nodequeue-dragdrop-reference-' . $form['#subqueue']['reference'],
+  );
+  drupal_add_tabledrag($table_id, 'order', 'sibling', 'node-position');
+  drupal_add_js(drupal_get_path('module', 'nodequeue') . '/nodequeue_dragdrop.js');
+
+  $reverse[str_replace('-', '_', $table_id)] = (bool) $form['#queue']['reverse'];
+  drupal_add_js(
+    array(
+      'nodequeue' => array(
+        'reverse' => $reverse,
+      )
+    ),
+    array(
+      'type' => 'setting',
+      'scope' => JS_DEFAULT,
+    )
+  );
+
+  // Render form as table rows.
+  $rows = array();
+  $counter = 1;
+  foreach (element_children($form) as $key) {
+    if (isset($form[$key]['title'])) {
+      $row = array();
+
+      $row[] = $form[$key]['#node']['field_map_marker_label']['und'][0]['value'];
+      $row[] = drupal_render($form[$key]['title']);
+      $row[] = drupal_render($form[$key]['author']);
+      $row[] = drupal_render($form[$key]['date']);
+      $row[] = drupal_render($form[$key]['position']);
+      $row[] = (!empty($form[$key]['edit'])) ? drupal_render($form[$key]['edit']) : '&nbsp;';
+      $row[] = drupal_render($form[$key]['remove']);
+      $row[] = array(
+        'data' => $counter,
+        'class' => array('position')
+      );
+
+      $rows[] = array(
+        'data'  => $row,
+        'class' => array('draggable'),
+      );
+    }
+
+    $counter++;
+  }
+  if (empty($rows)) {
+    //$rows[] = array(array('data' => t('No nodes in this queue.'), 'colspan' => 7));
+    $rows[] = array(array('data' => t('No nodes in this queue.'), 'colspan' => 8));
+  }
+
+  // Render the main nodequeue table.
+  $header = array(t('Marker'), t('Title'), t('Author'), t('Post Date'), t('Position'), array('data' => t('Operations'), 'colspan' => 2), t('Position'));
+  $output .= theme('table', array('header' => $header, 'rows' => $rows, 'attributes' => array('id' => $table_id, 'class' => $table_classes)));
+
+  return $output;
+}
