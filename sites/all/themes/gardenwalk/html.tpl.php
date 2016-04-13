d380a236ce8ce73573c5b95127c27de028db10cc
Jeff Schuler <jeff@websubstrate.com>
2016-04-13 16:43:51 -0600

    theme: removed 2015 from page template metatags.

--- sites/all/themes/gardenwalk/html.tpl.php
+++ sites/all/themes/gardenwalk/html.tpl.php
@@ -39,8 +39,8 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <meta name="keywords" content="cleveland,garden,walk,tour,community,urban,market,reimagining,flower,vegetable,neighborhood,tremont,ohio,city,tremont,larchmere,detroit,shoreway,glenville,old,brooklyn,centre,harvard,lee,miles,hough,slavic,village,st,clair,superior,west,park" />
-  <meta name="description" content="GardenWalk Cleveland is a free, self-guided tour of private gardens, community gardens, urban farms and more -- on July 11th and 12th, 2015 -- in neighborhoods of Cleveland, Ohio. Neighborhoods selected for 2015 are: Detroit Shoreway, Larchmere, Old Brooklyn, Slavic Village, and Westpark.." />
-  <meta name="abstract" content="GardenWalk Cleveland is a free, self-guided tour of private gardens, community gardens, urban farms and more -- on July 11th and 12th, 2015 -- in neighborhoods of Cleveland, Ohio." />
+  <meta name="description" content="GardenWalk Cleveland is a free, self-guided tour of private gardens, community gardens, urban farms and more in neighborhoods of Cleveland, Ohio." />
+  <meta name="abstract" content="GardenWalk Cleveland is a free, self-guided tour of private gardens, community gardens, urban farms and more in neighborhoods of Cleveland, Ohio." />
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/gardenwalk/images/apple-touch-icon-57.png" />
