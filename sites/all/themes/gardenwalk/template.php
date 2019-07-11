e09677eeaf08e1be83870204f3aabcf77bbe8ab1
Jeff Schuler <jeff@websubstrate.com>
2019-07-11 09:42:33 -0600

    Clean a couple things out of theme template.php.

--- sites/all/themes/gardenwalk/template.php
+++ sites/all/themes/gardenwalk/template.php
@@ -1,24 +1,5 @@
 <?php
 
-///*
-// * Custom Primary Links menu with link descriptions
-// */
-//function gardenwalk_menu_link__main_menu(array $variables) {
-//  $element = $variables['element'];
-//  $sub_menu = '';
-//
-//  if ($element['#below']) {
-//    $sub_menu = drupal_render($element['#below']);
-//  }
-//  // Menu item depth
-//  $depth = isset($variables['element']['#original_link']['depth']) ? $variables['element']['#original_link']['depth'] : -1;
-//  // Add description for top-level menu items only
-//  $description = ($depth==1) ? "<div class='description'>" . t($variables['element']['#localized_options']['attributes']['title']) . "</div>" : '';
-//  $title = "<div class='title'>" . $element['#title'] . "</div>";
-//  $output = l($title . $description, $element['#href'], array_merge($element['#localized_options'], array('html'=>TRUE)));
-//  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
-//}
-
 /**
  * Secondary menu: Menu Block following active Primary with Superfish
  */
@@ -52,9 +33,6 @@ function gardenwalk_preprocess_page(&$vars) {
       switch ($node->type) {
         case 'story':
         case 'garden':
-        case 'urban_farm':
-        case 'community_garden':
-        case 'reimagining_project':
           $types = node_type_get_types();
           $vars['type_title_prefix'] = check_plain($types[$node->type]->name) . ": ";
           break;
